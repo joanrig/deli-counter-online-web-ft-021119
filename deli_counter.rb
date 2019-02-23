bf5e5d66a7538c756763f48a2129efba02712478
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 01:11:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,26 @@
 require 'pry'
 
-katz_deli = []
-def line(katz_deli)
-  status = "The line is currently:"
-  if katz_deli.length > 0
-    katz_deli.each_with_index do |name, index|
-      status<< "#{index + 1}. #{name}"
-    end
-    puts status
-  else
-    puts "The line is currently empty."
-  end
-end
-
+# katz_deli = []
 # def line(katz_deli)
 #   status = "The line is currently:"
 #   if katz_deli.length > 0
 #     katz_deli.each_with_index do |name, index|
-#       status<< "#{index+1}. #{name}"
+#       status<< "#{index + 1}. #{name}"
 #     end
 #     puts status
 #   else
 #     puts "The line is currently empty."
 #   end
 # end
+
+def line(katz_deli)
+  status = "The line is currently:"
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, index|
+      status<< "#{index+1}. #{name}"
+    end
+    puts status
+  else
+    puts "The line is currently empty."
+  end
+end
