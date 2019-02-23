e69a3f036c31a0ec26f19bd0cd1cef914909bc36
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 01:00:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def line(katz_deli)
   if katz_deli.length > 0
-    katz_deli.map {|name, index| "#{index+1}. #{name}"}
+    line = (katz_deli.map {|name, index| "#{index+1}. #{name}"}).join("")
   else
     puts "The line is currently empty."
   end
