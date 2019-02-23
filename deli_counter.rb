46df14c3f50fbed1d0a766f1f4ab943b8bc1fdc5
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 01:11:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   status = "The line is currently:"
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
-      status<< "#{index + 1}. #{name} "
+      status<< "#{index + 1}. #{name}"
     end
     puts status
   else
