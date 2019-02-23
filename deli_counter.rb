49b66639dc5ba3519b02060636e7fd408b1b9e4c
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:56:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
+require 'pry'
 
 def line(katz_deli)
-  if katz_deli == nil
+  if katz_deli.length == 0
     puts "The line is currently empty."
+    binding.pry
   else
     line = katz_deli.map {|name, index| "#{index+1}. name"}.join(" ")
     puts "The line is currently #{line}."
