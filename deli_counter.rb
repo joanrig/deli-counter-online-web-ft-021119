454464eb934125249e0d7d594824218844189616
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:55:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli == nil
     puts "The line is currently empty."
   else
     line = katz_deli.map {|name, index| "#{index+1}. name"}.join(" ")
