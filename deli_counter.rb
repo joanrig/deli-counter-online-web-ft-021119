46546ed813165cccb752cef48fe4514d1b06c227
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 01:18:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, new_name)
   puts "Welcome, #{new_name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.length = 0
     puts "The line is empty."
   else
